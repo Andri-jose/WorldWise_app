fd68c0bcaf4e2f069477418d40f4368fd83a3b7e
Andri Salazar <andr-y2008@hotmail.com>
2025-05-13 20:05:46 -0300

    saved238

---
+++ src/context/FakeAuthContext.jsx
@@ -0,0 +1,72 @@
+import { useContext, useReducer } from "react";
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload.user,
+        isAuthenticated: true,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+}  
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+
+  const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState);
+
+
+  const login = (email, password) => {
+    // Simulate a login process
+    // console.log(`Logging in with ${username} and ${password}`);
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "LOGIN", payload: { user: FAKE_USER } });
+      console.log("Login successful");
+    } else {
+      console.log("Invalid credentials");
+    }
+  };
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+ 
+}
+
+export { AuthProvider, useAuth };
